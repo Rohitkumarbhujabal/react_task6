84959db68d7ca96df4fd1ddafe6e05dcd940b92c
Rohit Kumar Bhujabal <rohitkumarbhujabal@Rohits-MacBook-Air.local>
2023-10-16 15:52:19 +0530

    final

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles/header.css';
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+      <h1>Rohit Kumar Bhujabal</h1>
+      <p>Web Developer</p>
+      </div>
+      <img src='./img/Me.jpg' alt='img'/>
+    </header>
+  );
+};
+
+export default Header;
